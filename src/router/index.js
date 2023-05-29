453f59cea63cc9719ab736b2ffef547bd58166b5
peleg schlider <pelegschlider@pelegs-MBP-2.lan>
2023-05-29 14:59:11 +0300

    students activities view, vuex store - wip

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import StudentActivities from "@/views/StudentActivities.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'student-activities',
+    component: StudentActivities,
   },
   {
     path: '/about',
---
+++ src/views/StudentActivities.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="student-activities">
+    Student Activites
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'StudentActivities',
+}
+</script>
